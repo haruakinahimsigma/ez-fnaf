94dd965e798d19372974792d82ea0c6906d54668
haruakinahimsigma <haruakinahimsigma@gmail.com>
2026-07-23 08:48:03 +0900

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,12 @@
+const params = new URLSearchParams(location.search);
+const game = params.get("game");
 
+const links = {
+  fnaf1: "https://YOUR-FNAF1-LINK/",
+  fnaf2: "https://YOUR-FNAF2-LINK/",
+  fnaf3: "https://YOUR-FNAF3-LINK/",
+  fnaf4: "https://YOUR-FNAF4-LINK/",
+  fnaf5: "https://YOUR-FNAF5-LINK/"
+};
+
+document.write(`<iframe src="${links[game]}" class="frame"></iframe>`);
